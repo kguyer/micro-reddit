fa2692c9e3cf03f7edc1c10dd33ed5f9a25e2f97
kguyer <kguyer@harding.edu>
2023-07-17 16:13:17 -0500

    Add validations

--- db/migrate/20230717205621_create_posts.rb
+++ db/migrate/20230717205621_create_posts.rb
@@ -3,7 +3,7 @@ class CreatePosts < ActiveRecord::Migration[6.1]
     create_table :posts do |t|
       t.string :title
       t.string :body
-      t.refernces :user, null: false, foreign_key: true
+      t.references :user, null: false, foreign_key: true
 
       t.timestamps
     end
