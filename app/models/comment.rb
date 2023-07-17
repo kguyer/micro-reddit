2c0c35dcaa574679e6cb41eba8c75549cdc0b760
kguyer <kguyer@harding.edu>
2023-07-17 16:31:42 -0500

    Update validations

--- app/models/comment.rb
+++ app/models/comment.rb
@@ -1,6 +1,7 @@
 class Comment < ApplicationRecord
-    validates :user, presence: true
+    validates :user_id, presence: true
     validates :body, presence:true
+    validates :post_id, presence: true
 
     belongs_to :user
     belongs_to :post
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,4 +1,8 @@
 class User < ApplicationRecord
+    validates :username, presence: true, length: {minimum: 4}, uniqueness: true
+    validates :email, presence: true
+    validates :password, presence: true, length: {minimum: 8}
+    
     has_many :posts
     has_many :comments
 end
