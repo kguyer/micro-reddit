252f5d3fac4e204ed8c788aab7bbd82a87d2bf1a
Kate Guyer <kguyer@harding.edu>
2023-07-17 21:01:53 -0500

    move things around

--- app/models/comment.rb
+++ app/models/comment.rb
@@ -1,6 +1,6 @@
 class Comment < ApplicationRecord
-    validates :user_id, presence: true
     validates :body, presence:true
+    validates :user_id, presence: true
     validates :post_id, presence: true
 
     belongs_to :user
